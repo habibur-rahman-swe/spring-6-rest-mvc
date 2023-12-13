6dd2b729184fccdf076686990cb07018f5521982
habibur-rahman-swe <habibur0199mhr@gmail.com>
2023-12-13 18:17:42 +0600

    Initital Commete

---
+++ src/test/java/habib/springframework/springrest6mvc/Spring6RestMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package habib.springframework.springrest6mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6RestMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
