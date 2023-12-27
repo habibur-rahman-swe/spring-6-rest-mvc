c226171fb21c02f4881b46cc71d7b2eaea620f2f
habibur-rahman-swe <habibur0199mhr@gmail.com>
2023-12-27 11:56:40 +0600

    After adding Mapstruct Deps

---
+++ src/main/java/habib/springframework/springrest6mvc/mappers/BeerMapper.java
@@ -0,0 +1,8 @@
+package habib.springframework.springrest6mvc.mappers;
+
+import org.mapstruct.Mapper;
+
+@Mapper
+public class BeerMapper {
+
+}
---
+++ src/main/java/habib/springframework/springrest6mvc/mappers/CustomerMapper.java
@@ -0,0 +1,5 @@
+package habib.springframework.springrest6mvc.mappers;
+
+public class CustomerMapper {
+
+}
